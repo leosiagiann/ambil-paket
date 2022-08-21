0981617c947c689d96b2f6dea6e24a4fbea47921
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-22 00:36:09 +0700

    feat(agen): Proses Konfirmasi Paket Page

--- resources/views/agen/item/confirm.blade.php
+++ resources/views/agen/item/confirm.blade.php
@@ -137,7 +137,33 @@
                                 @endif
                             </td>
                             @endif
-                            <!-- make modal for download proof -->
+                            <!-- modal process{{ $item->id }} -->
+                            <div class="modal fade" id="process{{ $item->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Proses</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Apakah anda yakin ingin melanjutkan proses pengiriman?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Tidak</button>
+                                            <form action="" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-success">Ya</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end modal process{{ $item->id }} -->
+                            <!-- modal for download proof -->
                             <div class="modal fade" id="proof{{ $item->id }}" tabindex="-1" role="dialog"
                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
