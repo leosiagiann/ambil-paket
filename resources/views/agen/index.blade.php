96f52e9f9fb0e79a4dbf6e822470f6dcf4f9d614
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-25 01:28:54 +0700

    fix(agen): Fixing Dashboard

--- resources/views/agen/index.blade.php
+++ resources/views/agen/index.blade.php
@@ -10,25 +10,23 @@
 
     <div class="row">
         <div class="col-xl-3 col-md-6 mb-4">
-            <a href="{{ route('admin.agen') }}" style="text-decoration: none;">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Agen
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    {{ $agens }}
-                                </div>
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Agen
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-users fa-2x text-gray-300"></i>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                {{ $agens }}
                             </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
-            </a>
+            </div>
         </div>
 
         <div class="col-xl-3 col-md-6 mb-4">
@@ -54,7 +52,7 @@
         </div>
 
         <div class="col-xl-3 col-md-6 mb-4">
-            <a href="{{ route('admin.riwayat-pengiriman') }}" style="text-decoration: none;">
+            <a href="{{ route('agen.riwayat-pengiriman') }}" style="text-decoration: none;">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -76,7 +74,7 @@
         </div>
 
         <div class="col-xl-3 col-md-6 mb-4">
-            <a href="{{ route('admin.riwayat-pengiriman') }}" style="text-decoration: none;">
+            <a href="{{ route('agen.riwayat-pengiriman') }}" style="text-decoration: none;">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
