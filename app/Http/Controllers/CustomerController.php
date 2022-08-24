cb5858513324e2fbb14a3d4c1ffbfe5d1eb28952
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-25 01:42:53 +0700

    fix(customer): Fixing Dashboard

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 use App\Models\Profile;
 
 class CustomerController extends Controller
@@ -21,6 +22,34 @@ class CustomerController extends Controller
         return view('customer.index', [
             'title' => 'Dashboard',
             'page' => 'Dashboard',
+            'items' => $this->getCountItem(),
+            'itemCancels' => $this->getCountItemCancel(),
         ]);
     }
+
+    public function getCountItemCancel()
+    {
+        $items =  Item::where('status', 'rejected')
+        ->orWhere('status', 'canceled')
+        ->orWhere('status', 'not_process')
+        ->get();
+
+        $items = $items->filter(function ($item) {
+            return $item->user_id == auth()->user()->id;
+        });
+        
+        return $items->count();
+    }
+
+    public function getCountItem()
+    {
+        $items =  Item::where('status', 'done')
+        ->get();
+
+        $items = $items->filter(function ($item) {
+            return $item->user_id == auth()->user()->id;
+        });
+
+        return $items->count();
+    }   
 }
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -2,5 +2,56 @@
 @include('layouts.customer.sidebar')
 @include('layouts.navbar')
 @section('content')
-Customer
+<div class="container-fluid">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Beranda</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-xl-6 col-md-6 mb-4">
+            <a href="{{ route('customer.riwayat-pengiriman') }}" style="text-decoration: none;">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Paket Dikirimkan
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    {{ $items }}
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-truck fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-xl-6 col-md-6 mb-4">
+            <a href="{{ route('customer.riwayat-pengiriman') }}" style="text-decoration: none;">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Paket Dibatalkan
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                {{ $itemCancels }}
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-truck fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
 @endsection
