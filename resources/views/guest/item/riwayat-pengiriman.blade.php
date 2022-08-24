4ac9c555b4925768acc78440c1321f31d0f69b52
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-25 02:20:38 +0700

    feat(guest): Riwayart Pengiriman Paket Page

---
+++ resources/views/guest/item/riwayat-pengiriman.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.template')
+@include('layouts.sidebar')
+@include('layouts.navbar')
+@section('content')
+<div class="container-fluid">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Riwayat Pengiriman</h6>
+        </div>
+        <div class=" card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Resi</th>
+                            <th>Pengirim</th>
+                            <th>Penerima</th>
+                            <th>Harga</th>
+                            <th>Estimasi Waktu</th>
+                            <th>Status</th>
+                            <th>Catatan</th>
+                            <th>Pembayaran</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
