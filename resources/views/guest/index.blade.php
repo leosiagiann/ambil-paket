622fad05061512b7a6d4687435e1cff40016e505
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-25 02:01:15 +0700

    fix(guest) : Fixing Dashboard

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,5 +2,52 @@
 @include('layouts.sidebar')
 @include('layouts.navbar')
 @section('content')
-Guest
+<div class="container-fluid">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Selamat Datang Pengunjung</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-xl-6 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Paket Dikirimkan
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                0
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-6 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Paket Dibatalkan
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                            0
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-truck fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
