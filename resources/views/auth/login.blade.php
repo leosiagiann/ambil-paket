54c80fd15e42a87509353eec1eeeb595ba25b881
Leonardo Siagian <if319017@students.del.ac.id>
2022-08-25 21:20:40 +0700

    fix: Fixing Login & adding assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,106 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Ambil Paket - Login</title>
-
-    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('assets/auth/images/icons/favicon.ico') }}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/animate/animate.css') }}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/css-hamburgers/hamburgers.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/select2/select2.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/css/main.css') }}">
+<!--===============================================================================================-->
 </head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="{{ asset('assets/auth/images/img-01.png') }}" alt="IMG">
+				</div>
 
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <div class="row justify-content-center">
-
-            <div class="col-xl-8 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                                    </div>
-                                    @if (session()->has('success'))
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session()->get('success') }}
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                    </div>
-                                    @endif
-                                    @if (session()->has('login_error'))
-                                    <div class="alert alert-danger" role="alert">
-                                        {{ session()->get('login_error') }}
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                    </div>
-                                    @endif
-                                    <form class="user" action="/auth/login" method="post">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email"
-                                                class="form-control form-control-user @error('email') is-invalid @enderror"
-                                                id="exampleInputEmail" name="email" aria-describedby="emailHelp"
-                                                placeholder="kevin@gmail.com" autofocus value="{{ old('email') }}">
-                                            @error('email')
-                                            <span class="invalid-feedback ml-3" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password"
-                                                class="form-control form-control-user @error('password') is-invalid @enderror"
-                                                id="exampleInputPassword" name="password" placeholder="Password">
-                                            @error('password')
-                                            <span class="invalid-feedback ml-3" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            <i class="fas fa-sign-in-alt"></i>
-                                            Login
-                                        </button>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="{{ route('auth.register') }}">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <form class="user" action="{{ route('auth.login') }}" method="post">
+                    @csrf
+					<span class="login100-form-title">
+						Login
+					</span>
+                    
+                    @if (session()->has('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session()->get('success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
                     </div>
-                </div>
+                    @endif
+                    @if (session()->has('login_error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session()->get('login_error') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                    </div>
+                    @endif
 
-            </div>
+                    @error('email')
+                    <span class="text-danger ml-3" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+					<div class="wrap-input100">
+						<input class="input100" type="text" name="email" placeholder="Email" autofocus value="{{ old('email') }}">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true" @error('email') style="color: red;" @enderror></i>
+						</span>
+					</div>
 
-        </div>
+                    @error('password')
+                    <span class="text-danger ml-3" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+					<div class="wrap-input100">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true" @error('password') style="color: red;" @enderror></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit">
+							Login
+						</button>
+					</div>
 
-    </div>
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Do not have an account?
+						</span>
+						<a class="txt2" href="#">
+                            Sign Up
+						</a>
+					</div>
 
-    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
 
-    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+	
+<!--===============================================================================================-->	
+	<script src="{{ asset('assets/auth/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('assets/auth/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('assets/auth/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('assets/auth/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('assets/auth/vendor/tilt/tilt.jquery.min.js') }}"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="{{ asset('assets/auth/js/main.js') }}"></script>
 
 </body>
-
 </html>
